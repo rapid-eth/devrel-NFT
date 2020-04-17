b2d63487d2a4a47908d339ba0c34aa497917b86a
Joe Bernitt <j.b5286@gmail.com>
2020-04-17 11:33:56 -0400

    checkin

--- untruffle/compile.sh
+++ untruffle/compile.sh
@@ -5,6 +5,6 @@ OUTPUT_DIR=untruffle/compiled
 
 rm -r $OUTPUT_DIR
 mkdir -p $OUTPUT_DIR
-solc @openzeppelin=/Users/joeb/Workspace/rapid/cooganNFT/node_modules/@openzeppelin -o $OUTPUT_DIR --bin --abi --optimize contracts/*.sol 
+solc -o $OUTPUT_DIR --bin --abi --optimize contracts/*.sol 
 
 mkdir -p $CONTRACTS_BUILD_DIR
